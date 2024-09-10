4a0d8bd62a464eef256a68e90e2993b6cf7034d1
Chau Hoang Minh Tran <chtran@umass.edu>
2024-09-10 18:49:36 -0400

    Add files via upload

---
+++ app/(root)/(tabs)/details.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const PropertyDetails = () => {
+    return (
+        <SafeAreaView>
+            <Text>PropertyDetails</Text>
+        </SafeAreaView>
+    );
+};
+
+export default PropertyDetails;
---
+++ app/(root)/(tabs)/matches.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const Matches = () => {
+    return (
+        <SafeAreaView>
+            <Text>Matches</Text>
+        </SafeAreaView>
+    );
+};
+
+export default Matches
